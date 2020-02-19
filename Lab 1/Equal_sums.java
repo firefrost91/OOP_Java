536008fc385916c785dd8133909074087b7c9086
firefrost91 <49150875+firefrost91@users.noreply.github.com>
2020-02-20 02:29:35 +0530

    Create Equal_sums.java

---
+++ Lab 1/Equal_sums.java
@@ -0,0 +1,21 @@
+public class Main
+{
+	public static void main(String[] args) {
+	int p ; 
+	int i = 0 , j = 0 , flag = 0 ;
+	int A[] = new int[args.length];
+	int sum = Integer.parseInt(args[0]); 
+	for(i=1 ; i < args.length ; i ++){
+	    A[i]=Integer.parseInt(args[i]); 
+	}
+	for(i=1 ; i < args.length ; i ++){
+	   for(j=i ; j <args.length ; j++){
+	       if(sum==A[i]+A[j]){
+	           System.out.println(A[i]+" "+A[j]);
+	           flag++ ;  }
+	   }}
+	    if(flag==0){
+	        System.out.println("Not Found ");
+	    }
+	}
+}
