eea980da37f84d72708a4faf19a1a0e09eb7c1c9
firefrost91 <49150875+firefrost91@users.noreply.github.com>
2020-02-20 22:02:05 +0530

    Update ScannerEx.java

--- Lab 2/ScannerEx.java
+++ Lab 2/ScannerEx.java
@@ -4,6 +4,6 @@ public static void main(String args[]){
 Scanner s1 = new Scanner(System.in); 
 String i = s1.nextLine();
 int j = i.charAt(0); 
-System.out.println(j);
+System.out.println("ASCII value of " + i.charAt(0)+ " is :" + j);
 }
 }
