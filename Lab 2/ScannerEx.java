8d211f3ddba21c7a046114666035bf8b4926c16e
firefrost91 <49150875+firefrost91@users.noreply.github.com>
2020-02-20 21:58:44 +0530

    Create ScannerEx.java

---
+++ Lab 2/ScannerEx.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+public static void main(String args[]){
+Scanner s1 = new Scanner(System.in); 
+String i = s1.nextLine();
+int j = i.charAt(0); 
+System.out.println(j);
+}
+}
